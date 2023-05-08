100283697d9621011731e49516a127b6b6fcc5ce
Valkon <96625039+Nawija@users.noreply.github.com>
2023-05-08 17:47:01 +0200

    ll

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="pl">
             <head>
-                <link rel="shortcut icon" href="/icon.png"></link>
+                {/* <link rel="shortcut icon" href="/favicon.ico"></link> */}
             </head>
             <body className={inter.className}>
                 <Header />
